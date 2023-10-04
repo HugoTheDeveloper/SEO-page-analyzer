4361238d4f467dee4bc726b81066bb14dafeb852
bogdan <telioko@tutanota.com>
2023-10-04 20:21:04 +0300

    add index.html, update app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages, session
 
 
 app = Flask(__name__)
@@ -6,4 +6,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'Hi Reltih!'
+    messages = get_flashed_messages(with_categories=True)
+    if request.method == 'GET':
+        return render_template('index.html')
