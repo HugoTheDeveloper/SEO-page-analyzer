af16d35adbe4c96c919d66867167f60ccb2fef47
bogdan <telioko@tutanota.com>
2023-10-12 18:52:45 +0300

    update app, database_manager, basement.html, urls_list.html. add start_page.html, urls_checks_list.html. remove index.html.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages, session
 from validators import url as validate_url
-from validators import ValidationError
 from dotenv import load_dotenv
 import os
+from .database_manager import DbManager
+from urllib.parse import urlparse
 
 
 load_dotenv()
@@ -12,20 +13,49 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
 
+db = DbManager()
+
+
 @app.route('/', methods=['GET'])
 def start_page():
     messages = get_flashed_messages(with_categories=True)
-    if request.method == 'GET':
-        return render_template('index.html', messages=messages)
+    return render_template('start_page.html', messages=messages)
 
 
 @app.route('/urls', methods=['GET', 'POST'])
 def urls_list():
     if request.method == 'POST':
-        request_dict = request.form.to_dict()
-        url_to_check = request_dict.get('url')
-        flash('Указанный url не корректный', 'danger')
-        return render_template('index.html', previous_request=url_to_check, messages=messages)
+        url_to_check = request.form.get('url')
+        parsed_url = urlparse(url_to_check)
+        normalized_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+        if not validate_url(normalized_url):
+            flash('Указанный url не корректный!', 'danger')
+            return redirect(url_for('start_page')), 424
+        if db.is_url_in_bd(normalized_url):
+            flash('Страница указанного url уже существует', 'warning')
+            url_id = db.get_id_from_url(normalized_url)
+            return redirect(url_for('get_urls_checks_list', id=url_id)), 302
+        url = db.insert_url(normalized_url)
+        flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('get_urls_checks_list', id=url.id)), 302
+    if request.method == 'GET':
+        messages = get_flashed_messages(with_categories=True)
+        all_saved_urls = db.get_urls_list()
+        return render_template('urls_list.html', messages=messages,
+                               urls=all_saved_urls)
+
+
+@app.route('/urls/<int:id>', methods=['GET'])
+def get_urls_checks_list(id):
+    messages = get_flashed_messages(with_categories= True)
+    flag = db.is_url_id_in_bd(id)
+    if not flag:
+        flash('Запрашиваемая страница не найдена', 'warning')
+        return redirect(url_for('start_page')), 404
+    url_info = db.get_url_from_urls_list(id)
+    checks_list = db.get_url_from_urls_checks_list(id)
+    return render_template('urls_checks_list.html', messages=messages,
+                           url=url_info, checks_list=checks_list)
 
 
 
--- page_analyzer/database_manager.py
+++ page_analyzer/database_manager.py
@@ -3,7 +3,6 @@ from psycopg2.extras import NamedTupleCursor
 import os
 from dotenv import load_dotenv
 import datetime
-from urllib.parse import urlparse
 
 
 load_dotenv()
@@ -14,7 +13,7 @@ def init_connection():
     try:
         connection = psycopg2.connect(DATABASE_URL)
         return connection
-    except:
+    except Exception:
         print('Cant establish connection to database')
 
 
@@ -27,10 +26,61 @@ class DbManager:
         with init_connection() as conn:
             with get_cursor(conn) as cursor:
                 date = datetime.date.today()
-                url = urlparse(url)
-                normalized_url = url.scheme + url.netloc
                 cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)",
-                               (normalized_url, date))
+                               (url, date))
+                cursor.execute("SELECT * FROM urls WHERE name=%s", (url,))
+                url = cursor.fetchone()
+                return url
 
+    def get_urls_list(self):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute("SELECT * FROM urls ORDER BY id DESC;")
+                all_users = cursor.fetchall()
+                return all_users
 
+    def is_url_in_bd(self, url):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute("SELECT * FROM urls WHERE name=%s", (url,))
+                desired_url = cursor.fetchone()
+                if desired_url:
+                    return True
+                return False
+
+    def is_url_id_in_bd(self, url_id):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute('SELECT * FROM urls WHERE id=%s', (url_id,))
+                desired_url = cursor.fetchone()
+                if desired_url:
+                    return True
+                return False
+
+    def get_url_from_urls_list(self, url_id):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute("SELECT * FROM urls WHERE id=%s", (url_id,))
+                desired_url = cursor.fetchone()
+                if not desired_url:
+                    return False
+                return desired_url
+
+    def get_url_from_urls_checks_list(self, url_id):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute("SELECT * FROM urls_checks "
+                               "WHERE id=%s ORDER BY created_at DESC, id DESC",
+                               (url_id,))
+                checks_list = cursor.fetchall()
+                return checks_list
+
+    def get_id_from_url(self, url):
+        with init_connection() as conn:
+            with get_cursor(conn) as cursor:
+                cursor.execute("SELECT * FROM  urls WHERE name=%s", (url,))
+                url_id = cursor.fetchone()
+                if not url_id:
+                    raise ValueError('Database doesnt contain this url!')
+                return url_id.id
 
