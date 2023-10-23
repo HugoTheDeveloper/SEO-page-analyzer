bc5a66e72fe3b2778285de808590889864072e47
bogdan <telioko@tutanota.com>
2023-10-23 09:20:33 +0300

    update gitignore, pyproject.toml, database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS urls_checks;
 DROP TABLE IF EXISTS urls;
 CREATE TABLE urls(
 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 name varchar(255) NOT NULL,
 created_at date NOT NULL
 );
-DROP TABLE IF EXISTS urls_checks;
 CREATE TABLE urls_checks(
 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 url_id bigint REFERENCES urls (id),
